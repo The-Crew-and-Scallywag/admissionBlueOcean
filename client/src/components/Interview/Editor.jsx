9c37a30ddb380cc2c4f847ca1d88f4377ccd590b
Triston117 <roviiknaris@yahoo.com>
2023-07-06 14:21:42 -0700

    Added bracket closing and matching addon for code editor

--- client/src/components/Interview/Editor.jsx
+++ client/src/components/Interview/Editor.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import "codemirror/lib/codemirror.css";
-import CodeMirror from "codemirror";
+import "codemirror/addon/edit/closebrackets"; // Import the bracket closing addon
+import "codemirror/addon/edit/matchbrackets"; // Import the bracket matching addon
 import "codemirror/mode/htmlmixed/htmlmixed";
 import "codemirror/mode/css/css";
 import "codemirror/mode/javascript/javascript";
+import CodeMirror from "codemirror";
 
 const Editor = () => {
   const editorRef = useRef(null);
@@ -13,6 +15,8 @@ const Editor = () => {
     const editor = CodeMirror.fromTextArea(editorRef.current, {
       lineNumbers: true,
       mode: "htmlmixed",
+      autoCloseBrackets: true, // Enable auto-closing of brackets
+      matchBrackets: true, // Enable bracket matching
     });
 
     editor.on("change", (instance) => {
