87b7af850344e0d0db9bbfa0e8177dca127c13dc
ronnie-miller <jovi@Jovi-Desktop>
2023-07-05 20:41:42 -0700

    Fixed student info padding

---
+++ client/src/components/Dashboard/StudentInfo.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { TEMP_STUDENT_DATA } from "../Nav-Footer/utils";
+import { MdEditDocument, MdEditOff } from "react-icons/md";
+
+const StudentInfo = () => {
+  const [studentData, setStudentData] = useState(TEMP_STUDENT_DATA);
+  const [currentStudent, setCurrentStudent] = useState(TEMP_STUDENT_DATA[0]);
+  const [editMode, setEditMode] = useState(false);
+
+  const toggleEditMode = () => {
+    setEditMode(!editMode);
+  };
+
+  return (
+    <div className="grid grid-cols-1 w-[600px] mx-auto my-12">
+      <div className="text-white text-2xl text-center">
+        Selected Student:{" "}
+        <span className="text-accent">{currentStudent.Name}</span>
+      </div>
+      <div className="bg-bg rounded-lg m-2 p-2 shadow-lg shadow-black">
+        {editMode ? (
+          <MdEditOff
+            className="relative text-white float-right cursor-pointer text-xl"
+            onClick={toggleEditMode}
+          />
+        ) : (
+          <MdEditDocument
+            className="relative text-white float-right cursor-pointer text-xl"
+            onClick={toggleEditMode}
+          />
+        )}
+        <div className="mt-4 mb-12 grid grid-cols-1 md:grid-cols-2 gap-1 p-6">
+          {Object.entries(currentStudent)
+            .splice(1)
+            .map(([key, value]) => (
+              <div key={key} className="flex flex-col md:flex-row">
+                <div className="font-bold px-3 text-white text-lg">{key}:</div>
+                <div className="text-accent text-lg">{value}</div>
+              </div>
+            ))}
+        </div>
+        <div className="flex flex-col">
+          <button className="mx-auto text-white bg-secondary p-2 rounded-md mt-[-30px] mb-4 hover:bg-galv-orange transition-all duration-150 ease-in-out hover:scale-105">
+            Schedule an Interview
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StudentInfo;
