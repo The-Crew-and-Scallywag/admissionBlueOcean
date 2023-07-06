323eba6fa0e2c4a992390c55dc04cbea7321d485
zemitswasfuh <darrin.m.mosher@gmail.com>
2023-07-06 12:18:08 -0700

    sync with main

--- client/src/components/Interview/interview.jsx
+++ client/src/components/Interview/interview.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import Editor from "./Editor";
+
 const Interview = () => {
-  return <Editor />;
+  return (
+    <div className="grid grid-cols-4 gap-4 pt-8">
+      <div id="left-column" className="col-span-3">
+        <Editor />
+      </div>
+      <div id="right-column" className="col-span-1">
+        {/* Add your other components here */}
+      </div>
+    </div>
+  );
 };
+
 export default Interview;
