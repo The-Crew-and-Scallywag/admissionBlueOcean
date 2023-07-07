ac1938fb45150fa4eb4a44dd4dd59ccd72d4f248
mathomas605 <mathomas724@gamil.com>
2023-07-07 12:25:36 -0400

    added interview questions to seed sql

--- client/src/components/Interview/Question.jsx
+++ client/src/components/Interview/Question.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 export default function Question() {
   return (
-    <div id='question-container' className='text-center'>
-      <div id='question' className='text-white text-lg'>
+    <div id="question-container" className="text-center">
+      <div id="question" className="text-white text-lg">
         Question: Can you create an admissions service for Galvanize?
       </div>
     </div>
