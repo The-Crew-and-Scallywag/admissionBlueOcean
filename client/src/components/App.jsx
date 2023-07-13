3cc392034d43d752742ee7150cf9565f233a96e5
mathomas605 <mathomas724@gamil.com>
2023-07-13 12:37:18 -0400

    changes to questions and chalenges

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,7 +4,7 @@ import Dashboard from "./Dashboard/Dashboard.jsx";
 import Footer from "./Nav-Footer/Footer.jsx";
 import { Routes, Route } from "react-router-dom";
 import Login from "./Login/Login.jsx";
-import Interview from "./Interview/Interview.jsx";
+import Interview from "./Interview.jsx";
 
 const App = () => {
   return (
@@ -14,11 +14,7 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Dashboard />} />
           <Route exact path="/login" element={<Login />} />
-<<<<<<< HEAD
           <Route exact path="/Interview" element={<Interview />} />
-=======
-          <Route exact path="/interview" element={<Interview />} />
->>>>>>> refs/remotes/origin/main
         </Routes>
       </div>
       <Footer />
