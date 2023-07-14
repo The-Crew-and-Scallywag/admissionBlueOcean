e3d355306c296a3ec965f6974f77ba8d01981630
samsonb25 <samsonb25@yahoo.com>
2023-07-14 12:21:40 -0500

    working on adding fakeusers

---
+++ Server/src/scripts/bulkInsert.js
@@ -0,0 +1,23 @@
+import { faker } from "@faker-js/faker";
+import fs from "fs";
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: "postgres://samson:123@localhost:5432/blue_ocean",
+});
+
+const writableStream = fs.createWriteStream("data.csv");
+
+await pool.query("DELETE FROM users");
+
+for (let i = 0; i < 10_000; i++) {
+  const username = faker.internet.displayName();
+  const password = faker.internet.password({ length: 15 });
+  const email = faker.internet.email({ firstname: username });
+
+  writableStream.write(`${username},${email},${password}\n`);
+}
+
+writableStream.close();
+await pool.end();
