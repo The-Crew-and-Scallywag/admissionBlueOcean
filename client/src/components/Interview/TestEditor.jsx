1b28a1b618702bcb08ede6d560536dbf4066ec76
ronnie-miller <jovi@Jovi-Desktop>
2023-07-18 09:34:47 -0700

    styled output display

--- client/src/components/Interview/TestEditor.jsx
+++ client/src/components/Interview/TestEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Editor from "@monaco-editor/react";
 import * as Y from "yjs";
 import { WebrtcProvider } from "y-webrtc";
@@ -7,6 +7,7 @@ import axios from "axios";
 
 const TestEditor = ({ student, students, setStudent }) => {
   const editorRef = useRef(null); // Reference to the Monaco editor instance
+  const codeRef = useRef(null); // Reference to the code returned
   const [results, setResults] = useState([]); // State variable for the output of the code
   const [editorValue, setEditorValue] = useState("// Write your code here..."); // State variable for the initial value of the editor
   const [outputValues, setOutputValues] = useState([]); // State variable for storing the output values
@@ -15,10 +16,25 @@ const TestEditor = ({ student, students, setStudent }) => {
     const res = await axios.post("/api/run", {
       code: output,
     });
-    let returnValue = res.data.result[0];
-    console.log(returnValue);
+    let returnValue = res.data.result;
+
     setResults([...results, returnValue]); // Update the output state variable
   };
+
+  useEffect(() => {
+    if (codeRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = codeRef.current;
+
+      // Calculate the target scroll position
+      const targetScrollTop = scrollHeight - clientHeight;
+
+      // Scroll to the target position with smooth animation
+      codeRef.current.scrollTo({
+        top: targetScrollTop,
+        behavior: "smooth",
+      });
+    }
+  }, [results.length]);
 
   console.log(results);
 
@@ -44,7 +60,7 @@ const TestEditor = ({ student, students, setStudent }) => {
   return (
     <div>
       {student ? (
-        <div className="h-full w-full mx-auto my-20 items-center flex flex-row">
+        <div className="h-full w-full mx-auto my-20 items-center flex flex-col custom:flex-row">
           <div className="flex flex-col items-center">
             <h1 className="p-2 mb-2 text-xl font-bold tracking-wide text-white/70 underline-offset-2">
               Currently Interviewing:{" "}
@@ -54,7 +70,7 @@ const TestEditor = ({ student, students, setStudent }) => {
             </h1>
             <div
               id="editor"
-              className={`h-[700px] rounded-lg shadow-xl shadow-black bg-bg/20 border-2 border-secondary/50 transform transition-all duration-150 ease ${
+              className={`h-[800px] rounded-lg shadow-xl shadow-black bg-bg/20 border-2 border-secondary/50 transform transition-all duration-150 ease ${
                 results.length ? "w-[700px]" : "w-[900px]"
               }`}
             >
@@ -77,26 +93,52 @@ const TestEditor = ({ student, students, setStudent }) => {
                 }}
               />
             </div>
-            <div>
+            <div className="flex flex-row gap-4">
               <button
                 onClick={() => handleOutput(editorRef.current.getValue())}
                 className="bg-bg p-2 w-40 rounded-lg text-white/50 my-12  hover:scale-105 hover:bg-bg/70 hover:border-[1px] hover:border-accent transition-transform duration-300 ease-in-out shadow-lg shadow-black"
               >
                 Run
               </button>
+              {results.length > 0 && (
+                <button
+                  onClick={() => setResults([])}
+                  className="bg-bg p-2 w-40 rounded-lg text-white/50 my-12  hover:scale-105 hover:bg-bg/70 hover:border-[1px] hover:border-accent transition-transform duration-300 ease-in-out shadow-lg shadow-black"
+                >
+                  Clear
+                </button>
+              )}
             </div>
           </div>
           {results.length > 0 && (
-            <div className="bg-bg text-lg text-white font-bold m-4  min-w-[400px] max-h-[700px] text-center rounded-lg shadow-black shadow-xl h-full overflow-y-auto no-scrollbar relative top-[-50px]">
-              {results.length > 0 &&
-                results.map((item, index) => (
-                  <pre key={index} className="py-2 m-2 over">
-                    <span className="italic text-accent">
-                      Output {index + 1}:{" "}
-                    </span>
-                    {item}
-                  </pre>
-                ))}
+            <div className="text-center">
+              <div className="bg-bg p-4 text-lg text-center text-white font-normal m-4  min-w-[400px] rounded-lg shadow-black shadow-xl h-full ">
+                <h1 className="text-2xl text-white/70 font-bold self-center p-2 mb-2">
+                  Outputs:
+                </h1>
+                <div className="w-full border-b-2 border-accent"></div>
+                <div
+                  ref={codeRef}
+                  className="max-h-[700px] overflow-y-auto no-scrollbar h-full relative"
+                >
+                  {results.length > 0 &&
+                    results.map((item, index) => (
+                      <div
+                        key={index}
+                        className="bg-secondary p-4 my-4 rounded-lg shadow-lg shadow-black text-left"
+                      >
+                        <span className="italic text-accent font-bold">
+                          Output {index + 1}:{" "}
+                        </span>
+                        {item.map((code, index) => (
+                          <pre key={index} className="py-2 m-2 ">
+                            {JSON.stringify(code[0], null)}
+                          </pre>
+                        ))}
+                      </div>
+                    ))}
+                </div>
+              </div>
             </div>
           )}
         </div>
