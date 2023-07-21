1593647b7a62a9db16c17e13d0a971131b3c712f
ronnie-miller <jovi@Jovi-Desktop>
2023-07-20 20:34:25 -0700

    styled challenge

--- client/src/components/Interview/challenges/Challenge.jsx
+++ client/src/components/Interview/challenges/Challenge.jsx
@@ -25,7 +25,7 @@ export default function Challenge({
   };
 
   return (
-    <div className="flex flex-col justify-center pt-2 w-[700px] border-b-2 border-gray-500 relative">
+    <div className="flex flex-col justify-center pt-2 w-[700px] border-b-2 border-gray-500 relative ">
       {/* <Question1 /> */}
       <Answer
         question={question}
@@ -35,12 +35,12 @@ export default function Challenge({
       <textarea
         value={notes}
         onChange={handleNotesChange}
-        className="w-full h-20 p-4 border border-gray-300 rounded"
+        className="w-full h-[200px] p-4 border border-accent bg-bg rounded-lg shadow-lg shadow-black text-white/70 resize-none text-lg tracking-wide"
         placeholder="Notes..."
       ></textarea>
       <button
         onClick={handleNoteSubmit}
-        className=" bg-gray-600 w-[100px] rounded-md text-white text-center absolute right-[0] bottom-[-35px] hover:scale-[102%]"
+        className="bg-bg p-2 w-40 rounded-lg text-white/50 my-12  hover:scale-105 hover:bg-bg/70 hover:border-[1px] hover:border-accent transition-transform duration-300 ease-in-out shadow-lg shadow-black self-center"
       >
         Submit Note
       </button>
